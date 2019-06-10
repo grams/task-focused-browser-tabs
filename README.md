fa6b20576ee43c67f2a2f361cf06486e7ffc652b
Guillaume Rams <grams@users.noreply.github.com>
2019-06-10 19:25:45 +0200

    Added LGPL licensing info

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # task-focused-browser-tabs
 This chrome extension provides some kind of _git stash_ for all your open tabs.
 
+[![LGPL](https://img.shields.io/github/license/grams/task-focused-browser-tabs.svg?style=shield)](https://github.com/grams/task-focused-browser-tabs/blob/master/LICENSE)
 [![CircleCI](https://circleci.com/gh/grams/task-focused-browser-tabs/tree/master.svg?style=shield)](https://circleci.com/gh/grams/task-focused-browser-tabs/tree/master)
+[![Release](https://img.shields.io/github/release/grams/task-focused-browser-tabs.svg?style=shield&amp;label=latest%20release)](https://github.com/grams/task-focused-browser-tabs/releases/)
+
+See https://www.schwinl.net/task-focused-tabs-chrome-extension
--- extension/taskFocusedTabs.js
+++ extension/taskFocusedTabs.js
@@ -1,3 +1,19 @@
+/*
+    This file is part of task-focused-browser-tabs extension for chrome.
+
+    task-focused-browser-tabs is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    task-focused-browser-tabs is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with task-focused-browser-tabs.  If not, see <https://www.gnu.org/licenses/>.
+ */
 "use strict";
 
 
